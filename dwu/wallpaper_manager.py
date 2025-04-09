f4fd6c3de836cd9c32dea7365488f761cb447d96
starrieste <snowyeterna@gmail.com>
2025-04-09 16:46:05 -0400

    more None checks

--- dwu/wallpaper_manager.py
+++ dwu/wallpaper_manager.py
@@ -5,20 +5,19 @@ import time
 from requests_html import HTMLSession
 
 class WallpaperManager:
-    def __init__(self, root_url='https://wallpaper-a-day.com'): 
+    def __init__(self, root_url='https://wallpaper-a-day.com'):
         self.session = HTMLSession()
         self.root_url = root_url
         self.img_src = None
         self.filename = None
-        
         self.constant_check = False
-        
+
     def wallpaperCheckLoop(self):
         self.constant_check = True
         self.starting_time = time.time()
         self.last_check = 0
         self.total_auto_checks = 0
-        
+
         while self.constant_check:
             if time.time() - self.last_check >= 600:
                 self.huntSRC()
@@ -26,51 +25,73 @@ class WallpaperManager:
                 self.setWallpaper()
                 self.last_check = time.time()
                 self.total_auto_checks += 1
-                
+
     def getSaveSet(self, n=0):
         self.huntSRC(n)
         self.saveWallpaper()
         self.setWallpaper()
 
     def huntSRC(self, n=0):
-        r = self.session.get(self.root_url)
-        link = r.html.find('.post')[n].find('a')[0].attrs['href']
-        r = self.session.get(link)
-        if link.startswith('https://imgur.com'):
-            self.img_src = r.html.find('.image-placeholder')[0].attrs['src']
-        elif link.startswith('https://drive.google.com'):
-            self.img_src = 'https://drive.google.com/uc?id=' + link.split('/')[5]
-        else:
-            self.img_src = link
+        try:
+            r = self.session.get(self.root_url)
+            posts = r.html.find('.post')
+            if posts and n < len(posts):
+                link = posts[n].find('a')[0].attrs.get('href', None)
+                if link:
+                    r = self.session.get(link)
+                    if link.startswith('https://imgur.com'):
+                        placeholder = r.html.find('.image-placeholder')
+                        self.img_src = placeholder[0].attrs['src'] if placeholder else None
+                    elif link.startswith('https://drive.google.com'):
+                        self.img_src = 'https://drive.google.com/uc?id=' + link.split('/')[5]
+                    else:
+                        self.img_src = link
+            else:
+                logging.error(f"No post found at index {n}.")
+        except Exception as e:
+            logging.error(f"An error occurred during huntSRC: {str(e)}")
 
     def saveWallpaper(self, src=None):
+        src = src or self.img_src
         if src is None:
-            src = self.img_src
+            logging.error("No source provided for wallpaper.")
+            return
 
-        if src.endswith(('png', 'jpg', 'jpeg')):
-            ext = src.split('/')[-1].split('.')[-1]
-            filename = f'latest_wallpaper.{ext}'
-        else:
-            filename = 'latest_wallpaper.png'
+        try:
+            if src.endswith(('png', 'jpg', 'jpeg')):
+                ext = src.split('/')[-1].split('.')[-1]
+                filename = f'latest_wallpaper.{ext}'
+            else:
+                filename = 'latest_wallpaper.png'
 
-        response = self.session.get(src)
-        if response.status_code == 200:
-            with open(filename, 'wb') as f:
-                f.write(response.content)
-        else:
-            logging.error(f'Failed to retrieve image. Status code: {response.status_code}')
-            
-        self.filename = filename
+            response = self.session.get(src)
+            if response.status_code == 200:
+                with open(filename, 'wb') as f:
+                    f.write(response.content)
+                self.filename = filename
+            else:
+                logging.error(f'Failed to retrieve image. Status code: {response.status_code}')
+        except Exception as e:
+            logging.error(f"An error occurred during saveWallpaper: {str(e)}")
 
     def setWallpaper(self, path=None):
         path = path or self.filename
+        if path is None:
+            logging.error("No path provided for setting wallpaper.")
+            return
+
         if not os.path.isfile(path):
             logging.error(f'File not found: {path}')
             return
-        abs_path = os.path.abspath(path)
-        SPI_SETDESKWALLPAPER = 20
-        SPIF_UPDATEINIFILE = 0x01
-        SPIF_SENDCHANGE = 0x02
 
-        ctypes.windll.user32.SystemParametersInfoW(
-            SPI_SETDESKWALLPAPER, 0, abs_path, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE)
+        try:
+            abs_path = os.path.abspath(path)
+            SPI_SETDESKWALLPAPER = 20
+            SPIF_UPDATEINIFILE = 0x01
+            SPIF_SENDCHANGE = 0x02
+
+            ctypes.windll.user32.SystemParametersInfoW(
+                SPI_SETDESKWALLPAPER, 0, abs_path, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE
+            )
+        except Exception as e:
+            logging.error(f"An error occurred during setWallpaper: {str(e)}")
