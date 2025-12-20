a75e45414f5a450154a96409ff7e6bd0d0241a76
starrieste <starryeterna@gmail.com>
2025-12-20 17:02:56 -0500

    cleanup

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-from dwu.crash_handler import log_crash
 from dwu.app import *
-from dwu.logger import logger
 
 def main():
-    try:
-        run_app()
-    except Exception as e:
-        logger.exception("Unhandled exception occurred")
-        log_crash(e) # log separtely for crashes
+    run_app()
 
 if __name__ == "__main__":
     main()
